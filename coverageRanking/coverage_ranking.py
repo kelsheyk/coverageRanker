8cb86a366dc85253d40f4323eea3d6e5c09c91f8
King <kking@ausmac748899.ad.cirrus.com>
2018-07-24 14:54:41 -0500

    Report Output Parser & CL Arg Parser

--- coverageRanking/coverage_ranking.py
+++ coverageRanking/coverage_ranking.py
@@ -4,7 +4,9 @@
 import os, sys, re
 from os.path import isfile, join
 from subprocess import call
+import subprocess
 from pprint import pprint
+import argparse
 
 class coverageRanking():
     tests_by_class = {}
@@ -44,22 +46,65 @@ class coverageRanking():
                 for test in test_list:
                     # Run test with coverage
                     # py.test --cov=pyllist test/test_pyllist.py::testdllist::test_init_empty
-                    command = 'py.test --cov=' + self.cov_package + ' ' \
-                            + test_file + '::' + test_class + '::' + test
-                    call(command.split(' '))
+                    command = 'py.test --cov-report term-missing --cov=' + self.cov_package + ' ' \
+                            + test_file + '::' + test_class + '::' + test 
+                    cov_report = subprocess.run(command.split(' '), stdout=subprocess.PIPE)
+                    self.parseCovReport(cov_report.stdout.decode('utf-8'))
                     # Copy coverage to named data file
                     new_cov_file = '.coverage.' + test_class + '.' + test
                     call(['mv', '.coverage', 'covData/'+new_cov_file])
+
+    # Report Format: ['Name', 'Stmts', 'Miss', 'Cover', 'Missing']
+    def parseCovReport(self, cov_report):
+        self.file_coverage = {}
+        lines = cov_report.split('\n')
+        for line in lines:
+            # If python file
+            data = line.replace(', ', ',').split()
+            if len(data):
+                file_name = data[0]
+                if file_name[-3:] == '.py':
+                    self.file_coverage[file_name] = {}
+                    # Account for 100% format
+                    if ((len(data) > 3) and (data[3] == '100%')):
+                        self.file_coverage[file_name] = {
+                                'statements': data[1],
+                                'miss': '0',
+                                'cover': data[2],
+                                'missing': '',
+                        }
+                    # Account for 0%-99% format
+                    elif (len(data) > 4):
+                        self.file_coverage[file_name] = {
+                                'statements': data[1],
+                                'miss': data[2],
+                                'cover': data[3],
+                                'missing': data[4],
+                        }
+        pprint(self.file_coverage)
 
 
     def rankTests(self):
         call(['cd', self.test_dir])
         # TODO: iterate over .coverage files, combining & comparing
+        # Leverage self.file_coverage
         
 
 
 if __name__ == "__main__":
-    # TODO: pass in args from CL
-    ranker = coverageRanking('pyllist', '/Users/kking/SWTestProject/pypy-llist-master/test')
+    parser = argparse.ArgumentParser()
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument(
+        '--cov-package', dest='cov_package',
+       help='Package for which coverage is collected', required=True,
+    )
+    requiredNamed.add_argument(
+        '--test-dir', dest='test_dir',
+       help='Directory where tests are located', required=True,
+    )
+
+    args = parser.parse_args()
+    #ranker = coverageRanking('pyllist', '/Users/kking/SWTestProject/pypy-llist-master/test')
+    ranker = coverageRanking(args.cov_package, args.test_dir)
     ranker.parseTests()
     ranker.runTests()
