5a4d1131dbb588200d9f7bf6ec4c9f66874a7b2c
King <kking@ausmac748899.local>
2018-08-16 21:59:14 -0500

    Debug

--- coverageRanking/coverage_ranking.py
+++ coverageRanking/coverage_ranking.py
@@ -12,6 +12,7 @@ class coverageRanking():
     tests_by_class = {}
     file_coverage = {}
     file_line_representations = {}
+    num_smoke_tests = 5
 
     # Pass in the package that you are covering and tests dir
     def __init__(self, cov_package, test_dir='test'):
@@ -45,7 +46,11 @@ class coverageRanking():
         call(['mkdir', 'covData'])
         for test_file, test_classes in self.tests_by_class.items():
             for test_class, test_list in test_classes.items():
+                print('\n\n\n')
+                i =0
                 for test in test_list:
+                  i+=1
+                  if i<20:
                     full_test_name = test_file + '::' + test_class + '::' + test
                     # Run test with coverage
                     # py.test --cov=pyllist test/test_pyllist.py::testdllist::test_init_empty
@@ -97,29 +102,21 @@ class coverageRanking():
                     # Account for 100% format
                     if ((len(data) > 3) and (data[3] == '100%')):
                         line_coverage = self.parseMissingLines(file_name,'')
-                        self.file_coverage[test_name][file_name] = {
-                                'statements': data[1],
-                                'miss': '0',
-                                'cover': data[2],
-                                'missing': line_coverage,
-                        }
+                        self.file_coverage[test_name][file_name] = line_coverage
                     # Account for 0%-99% format
                     elif (len(data) > 4):
                         line_coverage = self.parseMissingLines(file_name, data[4])
-                        self.file_coverage[test_name][file_name] = {
-                                'statements': data[1],
-                                'miss': data[2],
-                                'cover': data[3],
-                                'missing': line_coverage,
-                        }
+                        self.file_coverage[test_name][file_name] = line_coverage
+        pprint(self.countOverallCovered(self.file_coverage[test_name]))
 
-    def countOverallCovered(self, test_cov_dir):
+
+    def countOverallCovered(self, test_cov_dict):
         covered_count = 0
-        for file_name,file_cov in test_cov_dir.items():
-            if 'missing' in file_cov.keys():
-                for line in file_cov['missing']:
+        for file_name,file_cov in test_cov_dict.items():
+            for line in file_cov:
                     if line:
                         covered_count+=1
+        #pprint(covered_count)
         return covered_count
 
     def coverage_merger(self, array1, array2):
@@ -135,11 +132,10 @@ class coverageRanking():
 
     def rankTests(self):
         call(['cd', self.test_dir])
-        # TODO: iterate over .coverage files, combining & comparing
         # Leverage self.file_coverage
         # At this point, self.file_coverage has:
-        # {test_name: {file_name: { 'missing': [T,T,T,F,F,T,...] }}}
-        # The missing array for each file has len = # lines in file
+        # {test_name: {file_name: [T,T,T,F,F,T,...] }}
+        # The array for each file has len = # lines in file
         #       and T=covered, F=missed
 
         # Start with test that covers most (heuristic)
@@ -154,6 +150,32 @@ class coverageRanking():
         # TODO: start with base_test, count non-overlapping coverage for each other test
         # merge test with biggest non-overlapping cov
         # repeat until num_smoke_tests
+        current_smoke_merge = {}
+        for file_name, file_cov in self.file_coverage[base_test].items():
+            current_smoke_merge[file_name] = file_cov
+        smoke_tests = [base_test]
+        highest_merge_count = self.countOverallCovered(self.file_coverage[base_test])
+        for i in range(0,self.num_smoke_tests):
+            for test_name, test_dict in self.file_coverage.items():
+                if test_name not in smoke_tests:
+                    merged_files = {}
+                    for file_name,file_cov in test_dict.items():
+                        merged_files[file_name] = self.coverage_merger(current_smoke_merge[file_name],file_cov)
+                    merge_count = self.countOverallCovered(merged_files)
+                    if merge_count >= highest_merge_count:
+                        highest_merge = merged_files
+                        highest_merge_count = merge_count
+                        highest_merge_test = test_name
+            #pprint(highest_merge_test)
+            #pprint(highest_merge_count)
+            smoke_tests.append(highest_merge_test)
+            current_smoke_merge = highest_merge
+        print('\n\n\n')
+        pprint(smoke_tests)
+        pprint(highest_merge_count)
+
+
+
 
 
 
@@ -170,7 +192,6 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    #ranker = coverageRanking('pyllist', '/Users/kking/SWTestProject/pypy-llist-master/test')
     ranker = coverageRanking(args.cov_package, args.test_dir)
     ranker.parseTests()
     ranker.runTests()
